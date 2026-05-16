b1b638697e484a4ecd9e39c64e70049ec2e79277
abdo <abdalrhman.hashm.cnik@gmail.com>
2026-05-16 19:04:45 +0200

    link the pages create shop , select comp

--- frontend/src/pages/trader/SelectShopPage.tsx
+++ frontend/src/pages/trader/SelectShopPage.tsx
@@ -1,8 +1,9 @@
+import SelectShop from "../../features/shop/components/SelectShop";
 
 export default function SelectShopPage() {
     return (
        <div className="min-w-0 flex-1 pt-16 md:pt-0">
-              
+               <SelectShop/>
                </div>
     )
 }
